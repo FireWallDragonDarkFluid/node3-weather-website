1ad30eb61910c7a9d78b30daa18b7ef92a9025c0
FireWallDragonDarkFluid <79576662+FireWallDragonDarkFluid@users.noreply.github.com>
2022-01-09 23:39:04 +0800

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const location = search.value
+    messageOne.innerText = 'Loading...'
+    messageTwo.innerText = ''
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                messageOne.innerText = data.error
+            }else{
+                messageOne.innerText = data.location
+                messageTwo.innerText = data.forecast
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+})
